be63d1288291768251edac14dff6ca73bf4097e4
Eduardo <eduardo.sa171006@gmail.com>
2025-04-05 16:45:10 -0600

    deje solo el prototipo de las funciones

--- menu.h
+++ menu.h
@@ -5,25 +5,9 @@ typedef struct{
     int t;
 } Pila;
 
-void inicializar_pila(Pila *p){
-    p->t = -1;
-}
+void inicializar_pila(Pila *p);
+int pila_vacia(Pila *p);
 
-int pila_vacia(Pila *p){
-    return p->t == -1;
-}
+int insertar_pila(Pila *p, int opcion);
 
-int insertar_pila(Pila *p, int opcion){
-    if (p->t < MAX_MENU - 1) {
-        p->opcion[++(p->t)] = opcion;
-        return 1;
-    }
-    return 0;
-}
-
-int pop(Pila *p) {
-    if (!pila_vacia(p)) {
-        return p->opcion[(p->t)--];
-    }
-    return -1;
-}
+int pop(Pila *p);
