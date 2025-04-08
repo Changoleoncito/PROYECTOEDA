30970aba85f1d52786a163a8f9fde251d7ce34c6
Eduardo <eduardo.sa171006@gmail.com>
2025-04-07 23:08:24 -0600

    funciones implementadas

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include "menu.h"
 #include "Funciones.h"
+
 int main(void){
+   
     int opcion,aux;
     char *nameaux;
+    float pagoTotal;
     Articulo *miArticulo;
     Cola *cola;
     cola = crearCola();
     miArticulo = crearArticulo();
-    char menu[]= "\n1)Imprimir \n2)Realizar pedido \n3)Ver mi carrito \n4)Editar Pedido\n5)Salir\n";
-
+    Usuario *usuario;
+    usuario = crearUsuario();
+    char menu[]= "\n1)Imprimir \n2)Añadir al carrito \n3)Ver mi carrito \n4)Editar Pedido\n5)Pagar\n6)Mi cuenta\n";
+    
     do{
         printf("%s",menu);
         scanf("%d",&opcion);
@@ -25,22 +30,52 @@ int main(void){
                 listar(*cola);
                 break;
             case 4:
-                listar(*cola);
-                printf("Que Articulo desea eliminar?: ");
-                scanf("%d",&aux);
-                nameaux = borrarArticulo(cola,&aux);
-                printf("El articulo:  %s fue eliminado",nameaux);
+                if(!colaVacia(*cola)){
+                    listar(*cola);
+                    printf("Que Articulo desea eliminar?: ");
+                    scanf("%d",&aux);
+                    nameaux = borrarArticulo(cola,&aux);
+                    printf("El articulo:  %s fue eliminado",nameaux);
+                }else{
+                    printf("\nNo hay articulos en el carrito\n");
+                }
+                    
+
+                
                 break;
-            case 5: //por ahora aqui pondre las funciones para liberar la momoria.
-                liberarClase(miClase);
-                liberarCiudadDatos(miCiudad);
-                liberarCola(miCola);
+            case 5: 
+                if(!colaVacia(*cola)){
+                    pagoTotal = pagar(cola);
+                    printf("El total es de $%.2f",pagoTotal);
+                    if(usuario->saldo < pagoTotal) printf("\nSaldo Insuficiente\n");
+                    else{
+                        printf("\nConfirme su pago: Presione 0 para cancelar y 1 para confirmar\n");
+                        scanf("%d",&aux);
+                        if(aux){
+                            usuario->saldo-=pagoTotal;
+                            printf("\nSaldo actual: $%.2f\n",pagoTotal);
+
+                        }
+                    }
+                }else{
+                    printf("\nNo hay articulos en el carrito\n");
+                }
+                break;
+            case 6:
+                imprimirDatos(*usuario);
+                break;
+            case 7:
+                //por ahora aqui pondre las funciones para liberar la momoria.
+                //liberarClase(miClase);
+                //liberarCiudadDatos(miCiudad);
+                liberarCola(cola);
                 printf("Memoria liberada. Saliendo del programa.\n");
                 break;
+
             default:
                 printf("Opcion no valida, intente de nuevo\n");
                 break;
         }
-    }while(opcion !=5);
+    }while(opcion !=7);
     return 0;
 }
